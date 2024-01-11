1a2a48e4ac012a6687ed57430170c15b61c20b87
Firverior02 <alexanderrunebou02@gmail.com>
2024-01-11 01:35:32 +0100

    feat: improve video page design, add head

--- app/src/main/java/com/loal/pillai/VideoActivity.java
+++ app/src/main/java/com/loal/pillai/VideoActivity.java
@@ -2,14 +2,22 @@ package com.loal.pillai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Picture;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.PictureDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.VideoView;
+
+import com.caverock.androidsvg.SVG;
+import com.caverock.androidsvg.SVGParseException;
 
 public class VideoActivity extends AppCompatActivity {
 
     VideoView videoView;
+    ImageView head;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +39,26 @@ public class VideoActivity extends AppCompatActivity {
 
         // Start video
         videoView.start();
+
+        // Head SVG
+        head = findViewById(R.id.head);
+
+        // Try to get head and render it
+        try {
+            SVG svg = SVG.getFromResource(this, R.raw.head);
+            Picture picture = svg.renderToPicture(300, 300);
+            Drawable drawable = new PictureDrawable(picture);
+
+            // Scale the drawable
+            int width = picture.getWidth();
+            int height = picture.getHeight();
+            float scaleFactor = 8.0f; // scale factor. Use 0.5f to half the size, 2.0f to double, etc.
+
+            drawable.setBounds(0, 0, (int)(width * scaleFactor), (int)(height * scaleFactor));
+
+            head.setImageDrawable(drawable);
+        } catch (SVGParseException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
